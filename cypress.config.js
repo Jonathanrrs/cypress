e3c4a2c0a7c0756670482f69c68f58b9b2df3f3e
Jonathanrrs <yoni_12rrs@hotmail.com>
2023-05-26 13:00:52 -0600

    ci: urlbase on giyhub actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '2fbdna',
+  projectId: "2fbdna",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "http://localhost:3000",
   },
 });
--- cypress/e2e/3-home/home.cy.js
+++ cypress/e2e/3-home/home.cy.js
@@ -2,7 +2,8 @@
 
 describe("testing home page", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/");
+    // cy.visit("http://localhost:3000/");
+    cy.visit("/");
   });
   it("show testing on cypress", () => {
     cy.get(".title-home").should("have.text", "Home");
